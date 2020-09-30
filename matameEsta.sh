021e19288e6183905ccc1180fcd24d4af9950a6c
slts <lotuflas@gmail.coom>
2020-09-30 12:45:38 +0200

    script mataProcesos

---
+++ matameEsta.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Escribe el nombre del proceso que quieres matar"
+read proceso
+MIPIDS=$( sudo ps -aux | grep "${proceso}" | awk '{ print $2}' )
+list=( $MIPIDS )
+for i in "${list[@]}"
+do
+	echo $i
+done
